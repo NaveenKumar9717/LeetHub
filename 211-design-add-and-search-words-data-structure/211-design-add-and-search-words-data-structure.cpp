7386d22d2cac11a401610c2acedd5ff0779f710c
Naveen Kumar <52369168+NaveenKumar9717@users.noreply.github.com>
2022-01-28 22:09:43 +0530

    Time: 365 ms (5.04%), Space: 111.7 MB (5.08%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,98 @@
+class Trie{
+private :
+    class TrieNode{
+        public :
+        TrieNode* children[26];
+        bool isend;
+        TrieNode(){
+            for(int i = 0 ; i <26;i++)
+                children[i] =  NULL;
+             	isend = false;
+        }
+    };
+
+TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    void insertH(string word, TrieNode*head){
+    	//cout<<word<<"<---word in insert "<<word.length()<<endl;
+      if(word.length() == 0){
+      	   head->isend = true;
+    		return;
+      } 
+        
+        int idx = 0;
+        idx = word[0] - 'a';
+        char x = 'a' + idx ;
+        //cout<<"insert :- "<<(x)<<endl;
+        if(head->children[idx] == NULL){
+            head->children[idx] = new TrieNode();	
+        }
+        
+       insertH(word.substr(1),head->children[idx]);
+     return;
+    }
+
+   bool searchH(string word, TrieNode*head){
+        // if(word.length() != 0 && word[0] =='.'){
+        //      return searchH(word.substr(1),head);
+        // } 
+       if(word.length() ==0){
+             //cout<<"Word end"<<endl; 
+          	return head->isend;
+          }    	
+       if(word[0] == '.'){
+      for(int i = 0;i<26;i++){
+      if(head->children[i] && searchH(word.substr(1),head->children[i]))
+               return true;
+           }
+           return false;
+       }
+       
+        int idx ;
+        
+        idx = word[0] - 'a';
+       // if(idx ==-51)
+        //cout<<idx<< "with word "<<word[0]<<endl;
+       // cout<<"its search"<<(x)<<endl;
+         if(head->children[idx] == NULL)
+         	return false;
+         return searchH(word.substr(1),head->children[idx]);
+    }
+
+ void insert(string word){
+    	cout<<word<<endl;
+    	insertH(word,root);
+    }
+    
+    bool search(string word) {
+    	 return searchH(word,root);
+    }
+    
+};
+
+class WordDictionary {
+public:
+    Trie* T;
+    WordDictionary() {
+       T = new Trie();    
+    }
+    
+    void addWord(string word) {
+        T->insert(word);
+    }
+    
+    bool search(string word) {
+       return  T->search(word);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
