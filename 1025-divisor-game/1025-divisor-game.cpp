b6b0d14b186670e56e315608f9326cb50c1ca243
Naveen Kumar <52369168+NaveenKumar9717@users.noreply.github.com>
2022-03-02 15:24:00 +0530

    Time: 8 ms (9.71%), Space: 6 MB (32.44%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -5,7 +5,7 @@ public:
       bool dp[N+1];
         memset(dp, false, N+1);
         for(int i=2; i <= N; i++)
-            for(int j = 1; j*j <= i;j++){
+            for(int j = 1; j < i;j++){
                 if(i % j == 0 && !dp[i - j])
                     dp[i] = true;
             }
