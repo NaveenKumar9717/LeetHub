bcd4ebb865b717545e3914f9eb343ad9875206b0
Naveen Kumar <52369168+NaveenKumar9717@users.noreply.github.com>
2022-03-14 23:49:48 +0530

    Time: 3 ms (95.14%), Space: 10.6 MB (25.77%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-       stack<string> st;
-        string res;
-        
-        for(int i = 0;  i<path.size(); ++i)
-        {
-            if(path[i] == '/')    
+        string ans ;
+        stack<string>stk;
+        int n = path.length();
+        for(int i  = 0; i < n;++i){
+            if(path[i] == '/')
                 continue;
-            string temp;
-			// iterate till we doesn't traverse the whole string and doesn't encounter the last /
-            while(i < path.size() && path[i] != '/')
-            {
-				// add path to temp string
+            
+            string temp ;
+            while(i < n && path[i] != '/'){
                 temp += path[i];
-                ++i;
+                i++;
             }
-            if(temp == ".")
-                continue;
-			// pop the top element from stack if exists
-            else if(temp == "..")
-            {
-                if(!st.empty())
-                    st.pop();
+            
+            if(temp == "."){
+              continue;
             }
-            else
-			// push the directory file name to stack
-                st.push(temp);
+            else if(temp == ".."){
+                if(!stk.empty())
+                    stk.pop();
+            }
+            else{
+              stk.push(temp);     
+            }
         }
         
-		// adding all the stack elements to res
-        while(!st.empty())
-        {
-            res = "/" + st.top() + res;
-            st.pop();
+        while(stk.empty() == false){
+            ans = "/" + stk.top() + ans;
+            stk.pop();
         }
         
-		// if no directory or file is present
-        if(res.size() == 0)
+        if(ans.length() == 0)
             return "/";
         
-        return res;
+        return ans;
     }
 };
