9d661f1f2ea83225ead6c153f6a7dc751712d8d4
Naveen Kumar <52369168+NaveenKumar9717@users.noreply.github.com>
2022-03-15 22:45:27 +0530

    Time: 28 ms (63.77%), Space: 12.4 MB (17.46%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        
+        stack<pair<char,int>>stk;
+        string ans ;
+        int n = s.length();
+        for(int i = 0 ; i < n;i++){
+            if(s[i] == '('){
+                stk.push(make_pair('(',i));
+            }
+            else if(s[i] == ')'){
+                if( stk.empty() == false && stk.top().first == '(' ){
+                    stk.pop();
+                }
+                else if(stk.empty()){
+                   s[i] = 'N';                    
+                }
+            }
+        }
+        
+        while(stk.empty() == false){
+            s[stk.top().second] = 'N';
+            stk.pop();
+        }
+            
+        for(int i = 0 ; i  <n ; i++){
+            if(s[i] != 'N')
+                ans += s[i];
+        }
+        return ans;
+    }
+};
