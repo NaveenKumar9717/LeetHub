89020d13da95ac31993682f5e2e0567d643a5879
Naveen Kumar <52369168+NaveenKumar9717@users.noreply.github.com>
2022-03-31 21:00:00 +0530

    Time: 12 ms (11.19%), Space: 9.4 MB (78.47%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,21 +1,63 @@
 class Solution {
 public:
+    int searchRow(vector<vector<int>>& matrix, int target){
+        int si = 0 ;
+        int ei = matrix.size()-1;
+        int m = matrix[0].size();
+        int ans = -1;
+        while(si <= ei){
+            int mid = (si + ei)/2;
+            if(matrix[mid][m-1] >= target){
+                ans = mid;
+                ei = mid-1;
+            }
+            else{
+                si = mid + 1;
+            }
+        }
+        return ans;
+    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-         int n  = matrix.size() ;
-     
-      int left  = 0 ; 
-      int right  = matrix[0].size() - 1 ;
+      int idx = searchRow(matrix,target);
+       if(idx == -1)
+        return false;
+       // cout<<idx<<endl;
+        int m = matrix[0].size();
+        int n = matrix.size();
         
-      while(left < n && right >=0 ){
+        int si = 0;
+        int ei = m - 1;
+        //cout<<"ei-->"<<ei<<endl;
+        int ans = -1;
+        while(si <= ei){
+            int mid = (si + ei)/2;
+            if(matrix[idx][mid] == target)
+                return true;
+            else if(matrix[idx][mid] < target)
+                si = mid + 1;
+            else
+                ei = mid - 1;
+            
+        }
+        return false;   
+        
+//       cout<<idx<<endl;  
+        
+//       int n  = matrix.size() ;
+//       int left  = 0 ; 
+//       int right  = matrix[0].size() - 1 ;
+        
+//       while(left < n && right >=0 ){
          
-         if(matrix[left][right] == target)
-             return true ;
-         else if(matrix[left][right] > target)
-             right-- ;
-         else
-             left++;
+//          if(matrix[left][right] == target)
+//              return true ;
+//          else if(matrix[left][right] > target)
+//              right-- ;
+//          else
+//              left++;
          
-     }   
-     return false ;  
+//      }   
+//      return false ;  
     }
 };
